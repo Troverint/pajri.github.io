2f332ba7c132ca87d63008ac53838a40250b11b9
Troverint <118240279+Troverint@users.noreply.github.com>
2022-11-22 12:12:15 +0700

    Add files via upload

---
+++ intro21.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{ 
+  string nama,tanggal,panggilan;
+  cout << "masukan nama : ";
+  cin >>nama;
+  cout << "nama panggilan : ";
+  cin >>panggilan;
+  cout << "masukan tanggal lahir : ";
+  cin >>tanggal;
+  cout << "jadi nama gua "<<nama;
+  cout << " lu juga bisa panggil gua "<<panggilan;
+  cout << " dan tangggal lahir gua "<<tanggal;
+  cout << " udah tod 🖕🖕";
+  
+ 
+  
+  
+  return 0;
+ 
+ 
+ 
+  
+  
+}
